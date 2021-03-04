26b1b4b8637470e49b71051a54bbaf4dbf181625
Tristan Charpentier <tristan_charpentier@hotmail.com>
2021-03-04 05:51:03 +0100

    Création du squelette Texture

---
+++ src/Texture.java
@@ -0,0 +1,24 @@
+import java.awt.image.BufferedImage;
+
+public class Texture {
+    public static int _id = 0;
+
+    public int id;
+    public String name;
+    public BufferedImage image;
+
+    public Texture(String name) {
+        //this.id = id;
+        this.name = name;
+    }
+
+    public Texture(String name, BufferedImage image) {
+        //this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
