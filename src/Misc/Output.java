0c154871163ad0b295c35b256ea8c8d11cdd4d99
Tristan Charpentier <tristan_charpentier@hotmail.com>
2021-04-14 16:25:54 +0200

    impl verbosité du program
    Signed-off-by: Tristan Charpentier <tristan_charpentier@hotmail.com>

--- src/Misc/Output.java
+++ src/Misc/Output.java
@@ -1,20 +1,29 @@
 package Misc;
 
 public class Output {
+    public static boolean error = true;
+    public static boolean warn = true;
+    public static boolean debug = true;
+    public static boolean info = true;
+
     // permet de donner des messages plus facilement lisible
     public static void errorln(String message) {
-        System.out.println("\u001B[31mERROR: " + message + "\u001B[0m");
+        if (Output.error)
+            System.out.println("\u001B[31mERROR: " + message + "\u001B[0m");
     }
 
     public static void warnln(String message) {
-        System.out.println("\u001B[33mWARN: " + message + "\u001B[0m");
+        if (Output.warn)
+            System.out.println("\u001B[33mWARN: " + message + "\u001B[0m");
     }
 
     public static void debugln(String message) {
-        System.out.println("\u001B[34mDEBUG: " + message + "\u001B[0m");
+        if (Output.debug)
+            System.out.println("\u001B[34mDEBUG: " + message + "\u001B[0m");
     }
 
     public static void infoln(String message) {
-        System.out.println(message);
+        if (Output.info)
+            System.out.println(message);
     }
 }
